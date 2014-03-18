049fe48b57fcf0b5c4726721a91e4773d933736e
Andrej Pavlovic <andrej.pavlovic@pokret.org>
2014-03-18 11:10:51 -0400

    #1508: Added test case for healthtracks_report_details error response.

--- api/v2/healthtracks_report_details.php
+++ api/v2/healthtracks_report_details.php
@@ -13,6 +13,27 @@ if (is_file($pathToReport)) {
     );
 }
 
+if (!empty($_COOKIE['response_override'])) {
+    $override = json_decode($_COOKIE['response_override'], true);
+
+    if (!empty($override['healthtracks_report_details'])) {
+        $values = $override['healthtracks_report_details'];
+
+        if (!empty($values['httpStatus'])) {
+            $httpStatus = $values['httpStatus'];
+        }
+
+        if (!empty($values['status'])) {
+            $response['status'] = $values['status'];
+            unset($response['healthtracks_info']);
+        }
+
+        if (!empty($values['error_msg'])) {
+            $response['error_msg'] = $values['error_msg'];
+        }
+    }
+}
+
 header($_SERVER['SERVER_PROTOCOL'] . ' ' . $httpStatus);
 header('Content-type: application/json');
 echo json_encode($response);
